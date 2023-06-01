33c32ff7f4ca494aaa3ca5d7dc6b570b875358e2
elenaeslin <srta.scobar@gmail.com>
2023-06-01 20:08:03 +0200

    Add about us section

--- edificapro/script.js
+++ edificapro/script.js
@@ -1,13 +1,24 @@
 const video = document.getElementById('video');
 const playBtn = document.getElementById('videoBtn');
+const aboutBtns = document.querySelectorAll('.about__btn');
+const aboutTexts = document.querySelectorAll('.about__texts .text');
 
 playBtn.addEventListener('click', () => {
-    if(playBtn.classList.contains('active')) {
+    if(playBtn.classList.contains('play')) {
         video.play();
     } else {
         video.pause();
     }
     playBtn.firstChild.classList.toggle('fa-play');
-    playBtn.firstChild.classList.toggle('fa-pause');
-    playBtn.classList.toggle('active');
-})
+    playBtn.classList.toggle('play');
+    playBtn.classList.toggle('pause');
+});
+
+aboutBtns.forEach(btn => btn.addEventListener('click', () => {
+    aboutBtns.forEach(allBtn => allBtn.classList.remove('active'));
+    aboutTexts.forEach(text => text.classList.remove('active'));
+
+    const text = document.getElementById(`${btn.innerHTML}`);
+    btn.classList.add('active');
+    text.classList.add('active');
+}));
