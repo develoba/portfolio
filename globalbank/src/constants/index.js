5763414bb4047084e32d5ba858efa874d1faeda2
elenaeslin <srta.scobar@gmail.com>
2023-07-27 16:22:13 +0200

    Setup project

---
+++ globalbank/src/constants/index.js
@@ -0,0 +1,197 @@
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Inicio",
+  },
+  {
+    id: "features",
+    title: "Características",
+  },
+  {
+    id: "product",
+    title: "Producto",
+  },
+  {
+    id: "clients",
+    title: "Clientes",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Recompensas",
+    content:
+      "Las mejores tarjetas de crédito ofrecen algunas combinaciones tentadoras de promociones y premios.",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "100% Seguro",
+    content:
+      "Tomamos medidas proactivas para asegurarnos de que su información y transacciones estén seguras.",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "Transferencia de saldo",
+    content:
+      "Una tarjeta de crédito con transferencia de saldo puede ahorrarte mucho dinero en cargos de intereses.",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "El dinero es solo una herramienta. Te llevará a donde desees, pero no te reemplazará como el conductor.",
+    name: "María García",
+    title: "Fundadora y Líder",
+    img: people01,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "El dinero facilita tu vida. Si tienes la suerte de tenerlo, eres afortunado.",
+    name: "Juan López",
+    title: "Fundador y Líder",
+    img: people02,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "Generalmente, son las personas involucradas en negocios relacionados con el dinero, las finanzas y el comercio internacional quienes son realmente ricas.",
+    name: "Víctor Rodríguez",
+    title: "Fundador y Líder",
+    img: people03,
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "Usuarios Activos",
+    value: "3800+",
+  },
+  {
+    id: "stats-2",
+    title: "Empresas",
+    value: "230+",
+  },
+  {
+    id: "stats-3",
+    title: "Transacciones",
+    value: "230M€+",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Enlaces útiles",
+    links: [
+      {
+        name: "Contenido",
+        link: "https://www.hoobank.com/content/",
+      },
+      {
+        name: "Cómo funciona",
+        link: "https://www.hoobank.com/how-it-works/",
+      },
+      {
+        name: "Crea",
+        link: "https://www.hoobank.com/create/",
+      },
+      {
+        name: "Explora",
+        link: "https://www.hoobank.com/explore/",
+      },
+      {
+        name: "Términos y Servicios",
+        link: "https://www.hoobank.com/terms-and-services/",
+      },
+    ],
+  },
+  {
+    title: "Comunidad",
+    links: [
+      {
+        name: "Centro de ayuda",
+        link: "https://www.hoobank.com/help-center/",
+      },
+      {
+        name: "Socios",
+        link: "https://www.hoobank.com/partners/",
+      },
+      {
+        name: "Sugerencias",
+        link: "https://www.hoobank.com/suggestions/",
+      },
+      {
+        name: "Blog",
+        link: "https://www.hoobank.com/blog/",
+      },
+      {
+        name: "Newsletters",
+        link: "https://www.hoobank.com/newsletters/",
+      },
+    ],
+  },
+  {
+    title: "Socios",
+    links: [
+      {
+        name: "Nuestros socios",
+        link: "https://www.hoobank.com/our-partner/",
+      },
+      {
+        name: "Conviértete en socio",
+        link: "https://www.hoobank.com/become-a-partner/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
+
+export const clients = [
+  {
+    id: "client-1",
+    logo: airbnb,
+  },
+  {
+    id: "client-2",
+    logo: binance,
+  },
+  {
+    id: "client-3",
+    logo: coinbase,
+  },
+  {
+    id: "client-4",
+    logo: dropbox,
+  },
+];
