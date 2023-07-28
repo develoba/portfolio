4e61c5ecc3911c15480a21e8c5b96825a45a94e0
elenaeslin <srta.scobar@gmail.com>
2023-07-28 14:19:19 +0200

    Add navbar and hero components

--- globalbank/src/components/GetStarted.jsx
+++ globalbank/src/components/GetStarted.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import styles from '../style'
+import { arrowUp } from '../assets'
 
-const GetStarted = () => {
-  return (
-    <div>GetStarted</div>
-  )
-}
+const GetStarted = () => (
+    <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+      <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
+        <p className='font-poppins font-medium text-[18px] leading-[23px]'>
+          <span className='text-gradient'>Empieza</span>
+        </p>
+        <div className={`${styles.flexStart} flex-row`}>
+          <p className='font-poppins font-medium text-[18px] leading-[23px] mr-1'>
+            <span className='text-gradient'>Ahora</span>
+          </p>
+          <img src={arrowUp} className='w-[23px] h-[23px] object-contain' alt='arrow' />
+        </div>
+      </div>
+    </div>
+)
 
 export default GetStarted
