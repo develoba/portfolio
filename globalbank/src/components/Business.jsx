684734fefbec49c5bfa39efbe7a95fc4cbdbfcd2
elenaeslin <srta.scobar@gmail.com>
2023-07-29 11:30:25 +0200

    add business section

--- globalbank/src/components/Business.jsx
+++ globalbank/src/components/Business.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { features } from '../constants'
+import styles, { layout } from '../style'
+import Button from './Button'
 
 const Business = () => {
   return (
-    <div>Business</div>
+    <section id='features' className={`${layout.section}`}>
+      <div className={`${layout.sectionInfo}`}>
+        <h2 className={`${styles.heading2}`}>Haces los negocios, <br className='sm:block hidden' /> nos encargamos del dinero</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Con la tarjeta de crédito adecuada, puede mejorar su vida financiera acumulando crédito, obteniendo recompensas y ahorrando dinero. Pero con cientos de tarjetas de crédito en el mercado.</p>
+
+        <Button styles='mt-10' />
+      </div>
+    </section>
   )
 }
 
