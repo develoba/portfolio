6c21f5527217f6350b0aba9a351791d79beb4ece
elenaeslin <srta.scobar@gmail.com>
2023-07-30 12:24:14 +0200

    Complete cta component

--- globalbank/src/components/CTA.jsx
+++ globalbank/src/components/CTA.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import styles from '../style'
+import Button from './Button'
 
-const CTA = () => {
-  return (
-    <div>CTA</div>
-  )
-}
+const CTA = () => (
+    <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
+      <div className='flex-1 flex flex-col'>
+        <h2 className={styles.heading2}>Prueba ahora nuestro servicio.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Todo lo que necesita para aceptar pagos con tarjeta y hacer crecer su negocio en cualquier lugar del planeta.
+        </p>
+      </div>
+
+      <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
+        <Button />
+      </div>
+    </section>
+)
+
 
 export default CTA
