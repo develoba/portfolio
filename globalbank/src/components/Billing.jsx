194d3144f6d98868c0bab4b395f145a19e32b1f1
elenaeslin <srta.scobar@gmail.com>
2023-07-30 11:00:49 +0200

    Complete billing component

--- globalbank/src/components/Billing.jsx
+++ globalbank/src/components/Billing.jsx
@@ -4,7 +4,7 @@ import styles, { layout } from '../style'
 
 
 const Billing = () => (
-    <section id='product' className={`${layout.sectionReverse}`}>
+    <section id='product' className={`${layout.sectionReverse} items-center`}>
       <div className={`${layout.sectionImgReverse}`}>
         <img src={bill} alt='bill' className='w-[100%] h-[100%] relative z-[5]' />
 
@@ -13,7 +13,15 @@ const Billing = () => (
       </div>
 
       <div className={`layout.sectionInfo`}>
+        <h2 className={styles.heading2}>Controla fácilmente <br className='sm:block hidden' /> tu facturación.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Controla fácilmente tus facturas y gastos con GlobalBank. Gestiona tus finanzas de manera intuitiva y eficiente.
+        </p>
 
+        <div className='flex flex-row flex-wrap sm:mt-10 mt-6'>
+          <img src={apple} alt='apple_store' className='w-[128px] h-[42px] object-contain mr-5 cursor-pointer' />
+          <img src={google} alt='google_play' className='w-[128px] h-[42px] object-contain  cursor-pointer' />
+        </div>
       </div>
     </section>
 )
