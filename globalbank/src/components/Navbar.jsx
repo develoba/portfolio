98a7b2eb5048620d814be11b434a709cfb54467c
elenaeslin <srta.scobar@gmail.com>
2023-07-30 13:24:09 +0200

    End project

--- globalbank/src/components/Navbar.jsx
+++ globalbank/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
 
       <ul className='list-none sm:flex hidden justify-end items-center'>
         {navLinks.map((nav, index) => (
-          <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0': 'mr-10'} text-white`}>
+          <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0': 'mr-10'} text-white hover:text-secondary`}>
             <a href={`#${nav.id}`}>
               {nav.title}
             </a>
@@ -31,7 +31,7 @@ const Navbar = () => {
           <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
             <ul className='list-none flex flex-col justify-end items-center flex-1'>
               {navLinks.map((nav, index) => (
-                <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0': 'mb-4'} text-white`}>
+                <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0': 'mb-4'} text-white hover:text-secondary`}>
                   <a href={`#${nav.id}`}>
                     {nav.title}
                   </a>
